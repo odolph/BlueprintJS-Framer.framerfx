ebb7ca1280163fced976bca5a7049fe4f1a45e83
odolph <paulhautot@gmail.com>
2019-11-22 10:21:48 -0800

    added state to radio group

--- code/inputs/RadioGroup.tsx
+++ code/inputs/RadioGroup.tsx
@@ -3,28 +3,39 @@ import * as System from "@blueprintjs/core";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { controls, merge } from "../generated/RadioGroup";
 import { withHOC } from "../withHOC";
+import { useManagedState } from "../utils/useManagedState"
+import { LabelPropertyControl } from "../utils/PropertyControls";
 
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
-
-const InnerRadioGroup: React.SFC = props => {
-  return <System.RadioGroup {...props} style={style} />;
-};
-
+const InnerRadioGroup: React.SFC<any> = ({ checked, label, items, ...props }) => {
+  const [currentlyChecked, setChecked] = useManagedState (checked)
+  return (
+    <System.RadioGroup {...props} value={currentlyChecked} onChange={e => setChecked(e.target["checked"])}>
+      {items.map(label => {
+        return (
+          <System.Radio key={label} value={currentlyChecked} onChange={e => setChecked(e.target["checked"])}>
+            {label}
+          </System.Radio>
+        )
+      })}
+    </System.RadioGroup>
+  )
+}
 export const RadioGroup = withHOC(InnerRadioGroup);
 
 RadioGroup.defaultProps = {
   width: 150,
-  height: 50
+  height: 110,
 };
 
 addPropertyControls(RadioGroup, {
+  items: {
+    type: ControlType.Array,
+    propertyControl: {
+      type: ControlType.String,
+    },
+    defaultValue: ["First", "Second", "Third"],
+  },
   disabled: merge(controls.disabled, {}),
   inline: merge(controls.inline, {}),
-  label: merge(controls.label, {}),
-  name: merge(controls.name, {}),
-  selectedValue: merge(controls.selectedValue, {}),
-  className: merge(controls.className, {})
+  ...LabelPropertyControl,
 });
