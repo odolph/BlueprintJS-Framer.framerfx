4640aa9c7b3569bd8a9f1ba55dc9ee5d266f48e5
odolph <paulhautot@gmail.com>
2019-11-26 10:57:06 -0800

    Shared Colors Added

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,3 +1,16 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #F5F8FA */
+    "default.white": "var(--token-41701ba9-b18a-4100-9976-78d4d6d36a4e, rgb(245, 248, 250))",
+    /** #137CBD */
+    "light.primary": "var(--token-ed213542-0925-44a7-bbd7-3217bcd9e4a1, rgb(19, 124, 189))",
+    /** #0F9960 */
+    "light.success": "var(--token-b393f478-d6c0-4516-81f7-11198dbcc1e4, rgb(15, 153, 96))",
+    /** #D9822B */
+    "light.warning": "var(--token-8a899eaf-9340-4c90-9475-7c12d323732c, rgb(217, 130, 43))",
+    /** #DB3737 */
+    "light.danger": "var(--token-34c63f6e-99a6-41b3-a5dd-cdb85e987f23, rgb(219, 55, 55))",
+})
