ba445fc5920726ea84c39ac2ff2e19a57f599590
odolph <paulhautot@gmail.com>
2019-11-26 19:43:48 -0800

    finished switch

--- code/other/Switch.tsx
+++ code/other/Switch.tsx
@@ -3,21 +3,23 @@ import * as System from "@blueprintjs/core";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { controls, merge } from "../generated/Switch";
 import { withHOC } from "../withHOC";
+import { useManagedState } from "../utils/useManagedState";
 
 const style: React.CSSProperties = {
   width: "100%",
   height: "100%"
 };
 
-const InnerSwitch: React.SFC = props => {
-  return <System.Switch {...props} style={style} />;
+const InnerSwitch: React.SFC<any> = ({ checked,  ...props }) => {
+  const [currentlyChecked, setChecked] = useManagedState (checked)
+  return <System.Switch {...props} style={style} checked={currentlyChecked} onChange={e => setChecked(e.target["checked"])} />;
 };
 
 export const Switch = withHOC(InnerSwitch);
 
 Switch.defaultProps = {
-  width: 150,
-  height: 50
+  width: 80,
+  height: 20
 };
 
 addPropertyControls(Switch, {
@@ -25,14 +27,10 @@ addPropertyControls(Switch, {
   innerLabel: merge(controls.innerLabel, {}),
   alignIndicator: merge(controls.alignIndicator, {}),
   checked: merge(controls.checked, {}),
-  children: merge(controls.children, {}),
   defaultChecked: merge(controls.defaultChecked, {}),
   disabled: merge(controls.disabled, {}),
   inline: merge(controls.inline, {}),
   label: merge(controls.label, {}),
   labelElement: merge(controls.labelElement, {}),
   large: merge(controls.large, {}),
-  tagName: merge(controls.tagName, {}),
-  className: merge(controls.className, {}),
-  placeholder: merge(controls.placeholder, {})
 });
