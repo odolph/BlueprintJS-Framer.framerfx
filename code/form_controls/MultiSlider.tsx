5ec8aab4cf438ad39188e7b0a04f901fa360409b
odolph <paulhautot@gmail.com>
2019-12-10 10:55:48 -0800

    first pass on Multi and Range Slider

--- code/form_controls/MultiSlider.tsx
+++ code/form_controls/MultiSlider.tsx
@@ -3,32 +3,33 @@ import * as System from "@blueprintjs/core";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { controls, merge } from "../generated/MultiSlider";
 import { withHOC } from "../withHOC";
+import { useManagedState } from "../utils/useManagedState";
 
 const style: React.CSSProperties = {
   width: "100%",
   height: "100%"
 };
 
-const InnerMultiSlider: React.SFC = props => {
-  return <System.MultiSlider {...props} style={style} />;
+const InnerMultiSlider: React.SFC<any> = ({ ...props}) => {
+  return <System.MultiSlider style={style} {...props} />
 };
 
 export const MultiSlider = withHOC(InnerMultiSlider);
 
 MultiSlider.defaultProps = {
   width: 150,
-  height: 50
+  height: 50,
+  value: [2,4,6,8],
 };
 
 addPropertyControls(MultiSlider, {
   defaultTrackIntent: merge(controls.defaultTrackIntent, {}),
   disabled: merge(controls.disabled, {}),
-  labelStepSize: merge(controls.labelStepSize, {}),
-  labelPrecision: merge(controls.labelPrecision, {}),
-  max: merge(controls.max, {}),
-  min: merge(controls.min, {}),
+  labelStepSize: merge(controls.labelStepSize, {defaultValue: 1, displayStepper: true }),
   showTrackFill: merge(controls.showTrackFill, {}),
-  stepSize: merge(controls.stepSize, {}),
+  labelPrecision: merge(controls.labelPrecision, { defaultValue: 0, displayStepper: true }),
+  max: merge(controls.max, { defaultValue: 10, displayStepper: true }),
+  min: merge(controls.min, { defaultValue: 0, displayStepper: true }),
+  stepSize: merge(controls.stepSize, { defaultValue: 1, displayStepper: true }),
   vertical: merge(controls.vertical, {}),
-  className: merge(controls.className, {})
 });
--- code/form_controls/RangeSlider.tsx
+++ code/form_controls/RangeSlider.tsx
@@ -17,17 +17,18 @@ export const RangeSlider = withHOC(InnerRangeSlider);
 
 RangeSlider.defaultProps = {
   width: 150,
-  height: 40
+  height: 50,
+  value: [2, 6],
 };
 
 addPropertyControls(RangeSlider, {
   disabled: merge(controls.disabled, {}),
-  labelStepSize: merge(controls.labelStepSize, {}),
-  labelPrecision: merge(controls.labelPrecision, {}),
-  max: merge(controls.max, {}),
-  min: merge(controls.min, {}),
-  showTrackFill: merge(controls.showTrackFill, {}),
-  stepSize: merge(controls.stepSize, {}),
+  labelStepSize: merge(controls.labelStepSize, {defaultValue: 1, displayStepper: true }),
+  showTrackFill: merge(controls.showTrackFill, {defaultValue: true}),
+  labelPrecision: merge(controls.labelPrecision, { defaultValue: 0, displayStepper: true }),
+  max: merge(controls.max, { defaultValue: 10, displayStepper: true }),
+  min: merge(controls.min, { defaultValue: 0, displayStepper: true }),
+  stepSize: merge(controls.stepSize, { defaultValue: 1, displayStepper: true }),
   vertical: merge(controls.vertical, {}),
-  className: merge(controls.className, {})
 });
+
